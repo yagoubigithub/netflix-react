8b25c18247a36c1c28ee153c87678968c02a10f8
Eytan Manor <eytan.manor@broadwing.io>
2021-07-16 22:39:40 +0100

    featured

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,27 +1,15 @@
-import NavBar from '../components/navbar/NavBar'
-import './home.scss'
+import Featured from "../components/featured/Featured";
+import NavBar from "../components/navbar/NavBar";
+import "./home.scss";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <NavBar />
+  return (
+    <div className="home">
+      <NavBar />
 
-            <img
-            width="100%"
-            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-          />
-         
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-         Eiusmod reprehenderit mollit sunt cillum officia commodo dolore ex aliqua labore amet voluptate deserunt aute. Eiusmod ex dolor cillum tempor consectetur tempor sunt sit. Aliqua mollit adipisicing pariatur mollit in ex. Culpa cupidatat amet elit laboris id enim est nisi commodo commodo deserunt ipsum cillum Lorem. Commodo consectetur deserunt consectetur ex occaecat do elit duis ad dolore ad. Eiusmod incididunt ea officia et Lorem anim reprehenderit laboris. Ex adipisicing elit tempor reprehenderit esse reprehenderit sunt amet cillum ipsum.
-        </div>
-    )
-}
+      <Featured type="movie" />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
