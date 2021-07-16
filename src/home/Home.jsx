4aec2352110bafc13b8baffaf9b5741e2cc2e7e0
Eytan Manor <eytan.manor@broadwing.io>
2021-07-16 21:01:10 +0100

    first commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,18 @@
+import NavBar from '../components/navbar/NavBar'
+import './home.scss'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <NavBar />
+
+            <img
+            width="100%"
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+        </div>
+    )
+}
+
+export default Home
